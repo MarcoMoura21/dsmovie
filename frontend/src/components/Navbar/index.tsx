7c8da114eac5f14956add7646ad4d7a666ee3817
MarcoMoura21 <mapmgi@hotmail.com>
2022-01-10 23:54:17 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as GithubIcon } from 'ascets/img/github.svg'
+import './style.css';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/MarcoMoura21" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+
+export default Navbar;
